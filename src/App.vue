83e93844f144d401f5720d25261cd0c36d043a56
namlh <hoainamle023@gmail.com>
2022-09-15 09:11:10 +0700

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<script setup>
+</script>
+
+<template>
+    <main class="main">
+        <div class="password-generator">
+            <h1>Password Generator</h1>
+            <div>
+                <p>PTx1d93fd9Xed</p>
+            </div>
+            <div>
+            </div>
+        </div>
+    </main>
+</template>
+
+<style lang="scss" scoped>
+    .main {
+        min-width: 100vw;
+        min-height: 100vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        .password-generator {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+
+            h1 {
+                color: var(--dark-grey);
+                font-family: 'JetBrains Mono';
+                font-style: normal;
+                font-weight: 700;
+                font-size: 16px;
+                line-height: 21px;
+                text-align: center;
+            }
+        }
+    }
+</style>
